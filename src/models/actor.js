1ef4629d5d5d7d80f0a19ee8d3b5cb2de1565d38
zurajanai <nckkvtn@gmail.com>
2019-11-14 19:15:46 +0200

    deleted unused model

--- src/models/actor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const actorSchema = new mongoose.Schema({
-    name: { type: String }
-});
-
-const Actor = mongoose.model('Actor', actorSchema);
-module.exports = Actor;
